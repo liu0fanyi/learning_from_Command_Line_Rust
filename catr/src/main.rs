abf98b7ac4e20b006c9f8f8dabbf97e02948ec91
liu0fanyi <liu0fanyi@tmail.com>
2023-10-06 00:18:57 +0800

    catr

---
+++ catr/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    // If the catr::get_args function returns an Ok(config) value, use Result ::and_then to pass the config to catr::run.
+    if let Err(e) = catr::get_args().and_then(catr::run) {// catr::run() {
+        eprintln!("{}", e);
+        std::process::exit(1);
+    }
+}
